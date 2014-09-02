12cf0d8fc278d1a395ff3dfd5067e55459039665
Elliott Butler <elliottmbutler@gmail.com>
2014-09-01 22:39:28 -0500

    Forgot to add this
    Signed-off-by: Elliott Butler <elliottmbutler@gmail.com>

---
+++ common/src/main/java/com/cogitareforma/hexrepublics/common/util/FilterEntityEventListener.java
@@ -0,0 +1,8 @@
+package com.cogitareforma.hexrepublics.common.util;
+
+import com.simsilica.es.ComponentFilter;
+
+public interface FilterEntityEventListener extends EntityEventListener
+{
+	public ComponentFilter getFilter( );
+}
