e8538a61f664c9b46278d17a767445342be9f121
Ryan <rgrier685@gmail.com>
2014-10-08 22:52:24 -0500

    Changed options so items have insets that space them out so sliders look correct now. Also added a base for changing input in options.
    Signed-off-by: Ryan <rgrier685@gmail.com>

--- client/src/main/java/com/cogitareforma/hexrepublics/client/OptionsScreen.java
+++ client/src/main/java/com/cogitareforma/hexrepublics/client/OptionsScreen.java
@@ -6,7 +6,6 @@ import com.jme3.app.Application;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
-import com.jme3.scene.Node;
 import com.simsilica.lemur.Axis;
 import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Button.ButtonAction;
@@ -15,6 +14,7 @@ import com.simsilica.lemur.Command;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.DefaultRangedValueModel;
 import com.simsilica.lemur.FillMode;
+import com.simsilica.lemur.Insets3f;
 import com.simsilica.lemur.Label;
 import com.simsilica.lemur.Panel;
 import com.simsilica.lemur.Slider;
@@ -47,6 +47,12 @@ public class OptionsScreen extends Screen
 	private Button graphicsButton;
 	private Button audioButton;
 	private Button inputButton;
+	private Button northButton;
+	private Button southButton;
+	private Button eastButton;
+	private Button westButton;
+	private Button chatButton;
+	private Button scoreboardButton;
 
 	@Override
 	public void cleanup( )
@@ -219,72 +225,140 @@ public class OptionsScreen extends Screen
 		audioButton.setFontSize( 17 * scale );
 		inputButton.setFontSize( 17 * scale );
 
-		graphics = new Container( new BoxLayout( Axis.Y, FillMode.Even ) );
+		graphics = new Container( new BoxLayout( Axis.Y, FillMode.None ) );
 		graphics.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .75f, 0 ) );
 		graphics.setBackground( new QuadBackgroundComponent( new ColorRGBA( .07f, .52f, .49f, 1 ) ) );
 		graphics.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * 0.8f, 0 );
 
-		audio = new Container( new BoxLayout( Axis.Y, FillMode.Even ) );
+		audio = new Container( new BoxLayout( Axis.Y, FillMode.None ) );
 		audio.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .75f, 0 ) );
 		audio.setBackground( new QuadBackgroundComponent( new ColorRGBA( .07f, .52f, .49f, 1 ) ) );
 		audio.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * 0.8f, 0 );
 
-		input = new Container( new BoxLayout( Axis.Y, FillMode.Even ) );
+		input = new Container( new BoxLayout( Axis.Y, FillMode.None ) );
 		input.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .75f, 0 ) );
 		input.setBackground( new QuadBackgroundComponent( new ColorRGBA( .07f, .52f, .49f, 1 ) ) );
 		input.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * 0.8f, 0 );
-
-		Label gLabel = new Label( "Graphics" );
-		gLabel.scale( scale );
-
-		Label aLabel = new Label( "Audio" );
-		aLabel.scale( scale );
-
-		Label iLabel = new Label( "Input" );
-		iLabel.scale( scale );
 
 		resLabel = new Label( "" );
 		resLabel.scale( scale );
 
 		res = new Slider( new DefaultRangedValueModel( 0, 15, 1 ), "glass" );
 		res.setName( "Resolution" );
+		res.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
+		// TODO make this good for both bigger and smaller sizes for everything.
 		resRef = res.getModel( ).createReference( );
 		resLabel.setText( "Resolution: " + String.valueOf( resRef.get( ) ) );
 
 		Checkbox fullscreen = new Checkbox( "FullScreen", "glass" );
 		fullscreen.setFontSize( 17 * scale );
+		fullscreen.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
 
 		quality = new Slider( new DefaultRangedValueModel( 0, 4, 4 ), "glass" );
 		quality.setName( "Quality" );
+		quality.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
 		qualityRef = quality.getModel( ).createReference( );
 
 		Checkbox vsync = new Checkbox( "VSync" );
 		vsync.setFontSize( 17 * scale );
+		vsync.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
+
+		Label mainLabel = new Label( "Main Volume" );
+		mainLabel.scale( scale );
+		Label musicLabel = new Label( "Music Volume" );
+		musicLabel.scale( scale );
+		Label soundLabel = new Label( "Sound Volume" );
+		soundLabel.scale( scale );
 
 		Slider mainVolume = new Slider( new DefaultRangedValueModel( 0, 100, 100 ), "glass" );
+		mainVolume.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
 		Slider musicVolume = new Slider( new DefaultRangedValueModel( 0, 100, 100 ), "glass" );
+		musicVolume.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
 		Slider soundsVolume = new Slider( new DefaultRangedValueModel( 0, 100, 100 ), "glass" );
+		soundsVolume.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
 		mainVolumeRef = mainVolume.getModel( ).createReference( );
 		musicVolumeRef = musicVolume.getModel( ).createReference( );
 		soundVolumeRef = soundsVolume.getModel( ).createReference( );
 
 		Checkbox console = new Checkbox( "Enable Dev Console: ", "glass" );
 		console.setFontSize( 17 * scale );
+		console.setInsets( new Insets3f( cam.getHeight( ) * .02f, 0, cam.getHeight( ) * .02f, 0 ) );
+
+		Container northContainer = new Container( new BoxLayout( Axis.X, FillMode.Even ) );
+		northContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .1f, 0 ) );
+		northContainer.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * .7f, 0 );
+		Label northLabel = new Label( "Move North " );
+		northLabel.scale( scale );
+		northContainer.addChild( northLabel );
+		northButton = new Button( "North Button" );
+		northButton.setFontSize( 17 * scale );
+		northContainer.addChild( northButton );
+		Container southContainer = new Container( new BoxLayout( Axis.X, FillMode.Even ) );
+		southContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .1f, 0 ) );
+		southContainer.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * .6f, 0 );
+		Label southLabel = new Label( "Move South " );
+		southLabel.scale( scale );
+		southContainer.addChild( southLabel );
+		southButton = new Button( "South Button" );
+		southButton.setFontSize( 17 * scale );
+		southContainer.addChild( southButton );
+		Container eastContainer = new Container( new BoxLayout( Axis.X, FillMode.Even ) );
+		eastContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .1f, 0 ) );
+		eastContainer.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * .5f, 0 );
+		Label eastLabel = new Label( "Move East " );
+		eastLabel.scale( scale );
+		eastContainer.addChild( eastLabel );
+		eastButton = new Button( "East Button" );
+		eastButton.setFontSize( 17 * scale );
+		eastContainer.addChild( eastButton );
+		Container westContainer = new Container( new BoxLayout( Axis.X, FillMode.Even ) );
+		westContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .1f, 0 ) );
+		westContainer.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * .4f, 0 );
+		Label westLabel = new Label( "Move West " );
+		westLabel.scale( scale );
+		westContainer.addChild( westLabel );
+		westButton = new Button( "West Button" );
+		westButton.setFontSize( 17 * scale );
+		westContainer.addChild( westButton );
+		Container chatContainer = new Container( new BoxLayout( Axis.X, FillMode.Even ) );
+		chatContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .1f, 0 ) );
+		chatContainer.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * .3f, 0 );
+		Label chatLabel = new Label( "Chat       " );
+		chatLabel.scale( scale );
+		chatContainer.addChild( chatLabel );
+		chatButton = new Button( "Chat Button" );
+		chatButton.setFontSize( 17 * scale );
+		chatContainer.addChild( chatButton );
+		Container scoreContainer = new Container( new BoxLayout( Axis.X, FillMode.Even ) );
+		scoreContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .9f, cam.getHeight( ) * .1f, 0 ) );
+		scoreContainer.setLocalTranslation( cam.getWidth( ) * .05f, cam.getHeight( ) * .2f, 0 );
+		Label scoreLabel = new Label( "Scoreboard" );
+		scoreLabel.scale( scale );
+		scoreContainer.addChild( scoreLabel );
+		scoreboardButton = new Button( "Score Button" );
+		scoreboardButton.setFontSize( 17 * scale );
+		scoreContainer.addChild( scoreboardButton );
 
 		swapContainer( graphics );
 
-		// graphics.addChild( gLabel );
 		graphics.addChild( resLabel );
 		graphics.addChild( res );
 		graphics.addChild( fullscreen );
 		graphics.addChild( quality );
 		graphics.addChild( vsync );
-		// audio.addChild( aLabel );
+		audio.addChild( mainLabel );
 		audio.addChild( mainVolume );
+		audio.addChild( musicLabel );
 		audio.addChild( musicVolume );
+		audio.addChild( soundLabel );
 		audio.addChild( soundsVolume );
-		// input.addChild( iLabel );
 		input.addChild( console );
+		input.addChild( northContainer );
+		input.addChild( southContainer );
+		input.addChild( eastContainer );
+		input.addChild( westContainer );
+		input.addChild( chatContainer );
+		input.addChild( scoreContainer );
 		getScreenNode( ).attachChild( middle );
 		getScreenNode( ).attachChild( top );
 
