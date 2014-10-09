6f19d1772bf76980cfd80c58090198d162547bb5
Ryan <rgrier685@gmail.com>
2014-10-09 14:49:16 -0500

    Fixed back button from not showing and removed old node.

--- client/src/main/java/com/cogitareforma/hexrepublics/client/NetworkScreen.java
+++ client/src/main/java/com/cogitareforma/hexrepublics/client/NetworkScreen.java
@@ -8,13 +8,17 @@ import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.scene.Node;
 import com.simsilica.lemur.Button;
+import com.simsilica.lemur.Command;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.Panel;
 import com.simsilica.lemur.TabbedPanel;
+import com.simsilica.lemur.Button.ButtonAction;
 import com.simsilica.lemur.component.QuadBackgroundComponent;
 
 public class NetworkScreen extends Screen
 {
+
+	private float scale;
 
 	@Override
 	public void cleanup( )
@@ -22,28 +26,37 @@ public class NetworkScreen extends Screen
 		// TODO Auto-generated method stub
 	}
 
+	@SuppressWarnings( "unchecked" )
 	@Override
 	public void initialize( ScreenManager screenManager, Application app )
 	{
 		super.initialize( screenManager, app );
 		Camera cam = screenManager.getApp( ).getCamera( );
-
-		Node networkNode = new Node( );
+		scale = cam.getHeight( ) * 0.0016f;
 
 		Panel background = new Panel( );
 		background.setBackground( new QuadBackgroundComponent( ColorRGBA.Gray ) );
 		background.setLocalTranslation( 0, cam.getHeight( ), 0 );
 		background.setPreferredSize( new Vector3f( cam.getWidth( ), cam.getHeight( ), 0 ) );
-		networkNode.attachChild( background );
+		getScreenNode( ).attachChild( background );
 
 		Container exitContainer = new Container( );
-		exitContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .1f, 50f, 0 ) );
+		exitContainer.setPreferredSize( new Vector3f( cam.getWidth( ) * .1f, 40f, 0 ) );
 		exitContainer.setBackground( new QuadBackgroundComponent( new ColorRGBA( 0, 0.5f, 0.5f, 0.5f ), 5, 5, 0.02f, false ) );
 		exitContainer.setLocalTranslation( cam.getWidth( ) * .9f, cam.getHeight( ), 0 );
 
 		Button back = new Button( "Back" );
+		back.setFontSize( 17*scale );
+		back.addCommands( ButtonAction.Click, new Command< Button >( )
+				{
+					public void execute( Button b )
+					{
+						System.out.println( "Exit Network Clicked: " + screenManager.setScreen( StartScreen.class ) );
+					}
+				} );
 
 		exitContainer.addChild( back );
+		getScreenNode( ).attachChild( exitContainer );
 
 		Container networkList = new Container( );
 		networkList.setPreferredSize( new Vector3f( cam.getWidth( ) * .8f, cam.getHeight( ) * .8f, 0 ) );
@@ -75,9 +88,7 @@ public class NetworkScreen extends Screen
 
 		networkList.addChild( serverTabs );
 
-		networkNode.attachChild( networkList );
-
-		getScreenNode( ).attachChild( networkNode );
+		getScreenNode( ).attachChild( networkList );
 	}
 
 	@Override
