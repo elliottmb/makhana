e78e91b804baa4c898a3ef333323c9c7c14c0de3
Ryan <rgrier685@gmail.com>
2014-10-19 19:01:29 -0500

    Added the new screens and a listener to open the console.
    Signed-off-by: Ryan <rgrier685@gmail.com>

--- client/src/main/java/com/cogitareforma/makhana/client/LemurTest.java
+++ client/src/main/java/com/cogitareforma/makhana/client/LemurTest.java
@@ -1,11 +1,17 @@
 package com.cogitareforma.makhana.client;
 
+import com.cogitareforma.makhana.client.ui.ConsoleScreen;
+import com.cogitareforma.makhana.client.ui.HudScreen;
+import com.cogitareforma.makhana.client.ui.LoadingScreen;
 import com.cogitareforma.makhana.client.ui.NetworkScreen;
 import com.cogitareforma.makhana.client.ui.OptionsScreen;
 import com.cogitareforma.makhana.client.ui.StartScreen;
 import com.cogitareforma.makhana.common.ui.ScreenManager;
 import com.jme3.app.FlyCamAppState;
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.ColorRGBA;
 import com.simsilica.lemur.Checkbox;
 import com.simsilica.lemur.GuiGlobals;
@@ -20,6 +26,8 @@ public class LemurTest extends SimpleApplication
 	private Styles styles;
 
 	private ScreenManager screenManager;
+
+	private ConsoleScreen console;
 
 	public static void main( String[ ] args )
 	{
@@ -58,12 +66,34 @@ public class LemurTest extends SimpleApplication
 		styles.getSelector( "header", "glass" ).set( "background", new QuadBackgroundComponent( new ColorRGBA( 0, 0.75f, 0.75f, 0.5f ) ) );
 		// BaseStyles.loadGlassStyle( ); // TODO: should be included but gets
 		// error: Groovy scripting engine not available.
-
+		console = new ConsoleScreen();
 		screenManager.addScreen( new StartScreen( ) );
 		screenManager.addScreen( new OptionsScreen( ) );
 		screenManager.addScreen( new NetworkScreen( ) );
+		screenManager.addScreen( new LoadingScreen( ) );
+		screenManager.addScreen( console );
+		screenManager.addScreen( new HudScreen( ) );
 		screenManager.setScreen( StartScreen.class );
+
+		inputManager.addMapping( "showConsole", new KeyTrigger( KeyInput.KEY_GRAVE ) );
+		inputManager.addListener( baseActionListener, "showConsole" );
 	}
+
+	private ActionListener baseActionListener = ( String name, boolean keyPressed, float tpf ) ->
+	{
+		if ( name.equals( "showConsole" ) && !keyPressed )
+		{
+			// TODO find out how to do this
+			if ( screenManager.showScreen(console) == true )
+			{
+				screenManager.showScreen( console );
+			}
+			else
+			{
+				screenManager.hideScreen( console );
+			}
+		}
+	};
 
 	@Override
 	public void simpleUpdate( float tpf )
