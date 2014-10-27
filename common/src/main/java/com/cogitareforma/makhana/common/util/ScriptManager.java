909928f3fc45e607315f16cb853c49d9f9f61ad1
Elliott Butler <elliottmbutler@gmail.com>
2014-10-26 20:32:13 -0500

    More work on #12
    Signed-off-by: Elliott Butler <elliottmbutler@gmail.com>

--- common/src/main/java/com/cogitareforma/makhana/common/util/ScriptManager.java
+++ common/src/main/java/com/cogitareforma/makhana/common/util/ScriptManager.java
@@ -2,20 +2,17 @@ package com.cogitareforma.makhana.common.util;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
-import groovy.util.GroovyScriptEngine;
-import groovy.util.ResourceException;
-import groovy.util.ScriptException;
 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -29,33 +26,45 @@ import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.control.customizers.ImportCustomizer;
 import org.codehaus.groovy.control.customizers.SecureASTCustomizer;
+import org.codehaus.groovy.runtime.InvokerHelper;
 import org.codehaus.groovy.syntax.Types;
 
+import com.cogitareforma.makhana.common.eventsystem.EntityEvent;
+import com.cogitareforma.makhana.common.eventsystem.EntityEventHandler;
 import com.google.common.collect.ImmutableList;
+import com.simsilica.es.EntityData;
+import com.simsilica.es.EntityId;
+import com.simsilica.es.EntitySet;
 
 public class ScriptManager
 {
-	private static final String[ ] defaultClassConstructionWhitelist = new String[ ]
+	private static final String[ ] defaultimportWhitelist = new String[ ]
 	{
 			Math.class.getName( ), Date.class.getName( ), Map.class.getName( ), List.class.getName( ), Set.class.getName( ),
 			ArrayList.class.getName( ), Arrays.class.getName( ), HashMap.class.getName( ), HashSet.class.getName( ),
-			BigDecimal.class.getName( ), String.class.getName( ), Boolean.class.getName( ), Boolean.TYPE.getName( )
+			BigDecimal.class.getName( ), String.class.getName( ), Boolean.class.getName( ), Boolean.TYPE.getName( ),
+			EntityEventHandler.class.getName( ), EntityEvent.class.getName( ), EntitySet.class.getName( ), EntityId.class.getName( ),
+			EntityData.class.getName( ), "java.lang.Math"
 	};
 
-	private static final String[ ] defaultReceiverWhitelist = new String[ ]
+	private final static String[ ] defaultReceiverWhitelist = new String[ ]
 	{
-			Math.class.getName( ), Integer.class.getName( ), Float.class.getName( ), Double.class.getName( ), Long.class.getName( ),
-			Short.class.getName( ), Character.class.getName( ), Byte.class.getName( ), Boolean.class.getName( ),
-			BigDecimal.class.getName( ), Arrays.class.getName( ), Date.class.getName( ), List.class.getName( ), Map.class.getName( ),
-			Set.class.getName( ), Object.class.getName( ), Integer.TYPE.getName( ), Long.TYPE.getName( ), Float.TYPE.getName( ),
-			Double.TYPE.getName( ), String.class.getName( ), Boolean.TYPE.getName( )
+			Math.class.getName( ), Integer.class.getName( ), "[I", "[[I", "[[[I", Integer.TYPE.getName( ), Float.class.getName( ), "[F",
+			"[[F", "[[[F", Float.TYPE.getName( ), Double.class.getName( ), "[D", "[[D", "[[[D", Double.TYPE.getName( ),
+			Long.class.getName( ), "[J", "[[J", "[[[J", Long.TYPE.getName( ), Short.class.getName( ), "[S", "[[S", "[[[S",
+			Character.class.getName( ), "[C", "[[C", "[[[C", Byte.class.getName( ), "[B", "[[B", "[[[B", Boolean.class.getName( ), "[Z",
+			"[[Z", "[[[Z", Boolean.TYPE.getName( ), BigDecimal.class.getName( ), Arrays.class.getName( ), Date.class.getName( ),
+			List.class.getName( ), Map.class.getName( ), Set.class.getName( ), Object.class.getName( ), String.class.getName( ),
+			"[[Ljava.lang.String;]", "java.lang.String", "[Ljava.lang.String;", InvokerHelper.class.getName( ),
+			EntityEventHandler.class.getName( ), EntityEvent.class.getName( ), EntitySet.class.getName( ), EntityId.class.getName( ),
+			EntityData.class.getName( )
 	};
 
 	private static final Integer[ ] defaultTokensWhiteList = new Integer[ ]
 	{
 			Types.PLUS, Types.MINUS, Types.MULTIPLY, Types.DIVIDE, Types.MOD, Types.POWER, Types.PLUS_PLUS, Types.MINUS_MINUS,
 			Types.COMPARE_EQUAL, Types.COMPARE_NOT_EQUAL, Types.COMPARE_LESS_THAN, Types.COMPARE_LESS_THAN_EQUAL,
-			Types.COMPARE_GREATER_THAN, Types.COMPARE_GREATER_THAN_EQUAL, Types.ASSIGNMENT_OPERATOR, Types.ASSIGN
+			Types.COMPARE_GREATER_THAN, Types.COMPARE_GREATER_THAN_EQUAL, Types.ASSIGN
 	};
 
 	/**
@@ -64,29 +73,36 @@ public class ScriptManager
 	private final static Logger logger = Logger.getLogger( ScriptManager.class.getName( ) );
 
 	private Binding binding;
-	private ImmutableList< String > constructionWhiteList;
+	private ImmutableList< String > importWhitelist;
 	private ImportCustomizer imports;
 	private ImmutableList< String > receiverWhiteList;
 	private SecureASTCustomizer secure;
 	private GroovyShell shell;
 	private ImmutableList< Integer > tokenWhiteList;
+	private List< File > fileScripts;
+	private List< String > stringScripts;
 
 	public ScriptManager( )
 	{
-		constructionWhiteList = ImmutableList.copyOf( defaultClassConstructionWhitelist );
+
+		importWhitelist = ImmutableList.copyOf( defaultimportWhitelist );
 		receiverWhiteList = ImmutableList.copyOf( defaultReceiverWhitelist );
 		tokenWhiteList = ImmutableList.copyOf( defaultTokensWhiteList );
 
 		imports = new ImportCustomizer( ).addStaticStars( Math.class.getName( ) );
 		secure = new SecureASTCustomizer( );
-		secure.setPackageAllowed( true );
-		secure.setClosuresAllowed( true );
+		secure.setPackageAllowed( false );
+		secure.setClosuresAllowed( false );
 		secure.setMethodDefinitionAllowed( true );
 
-		secure.setImportsWhitelist( constructionWhiteList );
+		secure.setImportsWhitelist( importWhitelist );
+
 		secure.setConstantTypesWhiteList( receiverWhiteList );
 		secure.setReceiversWhiteList( receiverWhiteList );
+
 		secure.setTokensWhitelist( tokenWhiteList );
+		secure.setStarImportsWhitelist( Collections.emptyList( ) );
+		secure.setStaticStarImportsWhitelist( Arrays.asList( "java.lang.Math" ) );
 
 		// Only remove the dangerous Expressions
 		secure.setExpressionsBlacklist( Arrays.asList( BytecodeExpression.class ) );
@@ -102,10 +118,17 @@ public class ScriptManager
 
 		shell = new GroovyShell( binding, compilerConfig );
 
+		fileScripts = new LinkedList( );
+		stringScripts = new LinkedList( );
+	}
+
+	public Object run( File file, List arguments )
+	{
+		Object eval = null;
 		// TODO: Look into replacing Shell with GroovyScriptEngine
 		try
 		{
-			shell.run( new File( "test.groovy" ), Collections.emptyList( ) );
+			eval = shell.run( file, arguments );
 		}
 		catch ( CompilationFailedException e )
 		{
@@ -117,9 +140,11 @@ public class ScriptManager
 			// TODO Auto-generated catch block
 			e.printStackTrace( );
 		}
+
+		return eval;
 	}
 
-	public Object evaluateScript( String script )
+	public Object evaluate( String script )
 	{
 		Object eval = null;
 		try
@@ -133,7 +158,7 @@ public class ScriptManager
 		return eval;
 	}
 
-	public boolean executeScript( String script )
+	public boolean execute( String script )
 	{
 		try
 		{
